ccbf5a6aa251fc1479ca8f5f907fe61c155d48f6
Arata Kagan <aratakagan@gmail.com>
2019-04-19 23:33:04 -0700

    Added server file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require('path');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use("/", (req, res, next) => {
+    res.send("Welcome to Qweerty Gamers");
+})
+
+// app.get('/api/hello', (req, res) => {
+//     res.send({express: 'Hello from Express'});
+// });
+
+// Serve static assets if it's in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
