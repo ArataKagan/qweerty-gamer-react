56d781d14a1f625edcec2ee2c0eae3c7183129de
Arata Kagan <aratakagan@gmail.com>
2019-04-29 14:41:31 -0700

    News article title setup

--- client/src/Components/Landing/News.js
+++ client/src/Components/Landing/News.js
@@ -1,51 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {
-    Carousel,
-    CarouselItem,
-    CarouselControl,
-    CarouselIndicators,
-    CarouselCaption
-  } from 'reactstrap';
 
 class News extends Component {
     constructor(props){
         super(props);
         this.state = { activeIndex: 0,
                         news: [],
-                        newsObj: {} }
-        this.next = this.next.bind(this);
-        this.previous = this.previous.bind(this);
-        this.goToIndex = this.goToIndex(this);
-        this.onExiting = this.onExiting(this);
-        this.onExited = this.onExited(this);
-    }
+                        author: [],
+                        title: [],
+                        url: [] }
+        }
 
-    onExiting(){
-        this.animating = true;
-    }
-
-    onExited() {
-        this.animating = false;
-    }
-
-    next() {
-        if (this.animating) return;
-        const nextIndex = this.state.activeIndex === this.state.newsObj.length - 1 ? 0 : this.state.activeIndex + 1;
-        this.setState({ activeIndex: nextIndex });
-    }
-
-    previous() {
-        if (this.animating) return;
-        const nextIndex = this.state.activeIndex === 0 ? this.state.newsObj.length - 1 : this.state.activeIndex - 1;
-        this.setState({ activeIndex: nextIndex });
-    }
-
-    goToIndex(newIndex) {
-        if (this.animating) return;
-        this.setState({ activeIndex: newIndex });
-    }
-   
+  
     componentDidMount(){
         axios.get('https://newsapi.org/v2/everything?q=lgbt&apiKey=e89e81f136f343719303e1f4d0fdf9c6')
             .then(response => {
@@ -58,38 +24,25 @@ class News extends Component {
     render(){
         
         for(let i=0; this.state.news.length > i; i++){
-           this.state.newsObj[i] = {"author": this.state.news[i].author,
-                         "title": this.state.news[i].title,
-                         "url": this.state.news[i].url
-                        }
+            this.state.author.push(this.state.news[i].author);
+            this.state.title.push(this.state.news[i].title);
+            this.state.url.push(this.state.news[i].url);
         }
-        console.log(this.state.newsObj);
+
+        var title = this.state.title.map((item, index) => {
+            const authors = this.state.author[index];
+            const urls = this.state.url[index];
+            return (
+                <div>
+                    <a href={urls}><p>{item}</p></a>
+                    <p>{authors}</p>
+                </div>
+            )
+        })
         
-
-        // const slides = this.state.newsObj.map((item) => {
-        //     return (
-        //         <Carousel
-        //         activeIndex={this.state.activeIndex}
-        //         next={this.next}
-        //         previous={this.previous}
-        //         >
-        //             <CarouselIndicators items={this.state.newsObj} onClickHandler={this.goToIndex} />
-        //             <CarouselItem
-        //                 onExiting={this.onExiting}
-        //                 onExited={this.onExited}
-        //                 key={item.url}
-        //             >
-        //                 <CarouselCaption captionText={item.title} captionHeader={item.author} />
-        //             </CarouselItem>
-        //             <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-        //             <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-        //         </Carousel>
-        //     )
-        // })
-
         return(
-            <div className="newsAPI">
-                {/* {slides} */}
+            <div>
+                {title}
             </div>
         )
     }
