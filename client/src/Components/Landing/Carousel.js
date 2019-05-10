350c7bace8e7a69722e05360e922bdf917d753e2
Arata Kagan <aratakagan@gmail.com>
2019-05-09 20:17:43 -0700

    removed zoom effect from carousel component

--- client/src/Components/Landing/Carousel.js
+++ client/src/Components/Landing/Carousel.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import Carousel from 'nuka-carousel';
-import Zoom from 'react-reveal/Zoom'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Slider extends Component {
     render() {
         return (
             <div className='Carousel'>
-                <Zoom>
-                    <Carousel>
-                        <img src="https://i.imgur.com/1EouPxr.png" alt='logo'/>
-                        <img src='https://i.imgur.com/hLjrsgZ.png' alt='image-two' />
-                    </Carousel>
-                </Zoom>
+                <Carousel>
+                    <img src="https://i.imgur.com/1EouPxr.png" alt='logo'/>
+                    <img src='https://i.imgur.com/hLjrsgZ.png' alt='image-two' />
+                </Carousel>
             </div>
         )
     }
