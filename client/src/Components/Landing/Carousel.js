21b607a91c7f5f30e44a0ea5ca0876cfeefb5d55
Arata Kagan <aratakagan@gmail.com>
2019-05-09 17:34:56 -0700

    finished News API rendering

--- client/src/Components/Landing/Carousel.js
+++ client/src/Components/Landing/Carousel.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Carousel from 'nuka-carousel';
+import Zoom from 'react-reveal/Zoom'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Slider extends Component {
     render() {
         return (
             <div className='Carousel'>
-                <Carousel>
-                    <img src="https://i.imgur.com/1EouPxr.png" alt='logo'/>
-                    <img src='https://i.imgur.com/hLjrsgZ.png' alt='image-two' />
-                </Carousel>
+                <Zoom>
+                    <Carousel>
+                        <img src="https://i.imgur.com/1EouPxr.png" alt='logo'/>
+                        <img src='https://i.imgur.com/hLjrsgZ.png' alt='image-two' />
+                    </Carousel>
+                </Zoom>
             </div>
         )
     }
--- client/src/Components/Landing/News.js
+++ client/src/Components/Landing/News.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import makeCarousel from 'react-reveal/makeCarousel';
+import Slide from 'react-reveal/Slide';
+import TextLoop from 'react-text-loop';
+import styled, { css } from 'styled-components';
 
 class News extends Component {
     constructor(props){
         super(props);
         this.state = { activeIndex: 0,
-                        news: [],
-                        author: [],
-                        title: [],
-                        url: [] }
+                        news: [] 
+                    }
         }
 
   
@@ -22,28 +24,43 @@ class News extends Component {
     }
 
     render(){
+        const title = [];
+        const url = [];
+        const author = [];
+        
+
+        // const Container = styled.div`
+        //     border: 1px solid red;
+        //     position: relative;
+        //     overflow: hidden;
+        //     width: 300px;
+        //     height: 150px;
+        //     `;
+        // const CarouselUI = ({ children }) => <Container>{children}</Container>
+        // const Carousel = makeCarousel(CarouselUI);
         
         for(let i=0; this.state.news.length > i; i++){
-            this.state.author.push(this.state.news[i].author);
-            this.state.title.push(this.state.news[i].title);
-            this.state.url.push(this.state.news[i].url);
+            author.push(this.state.news[i].author);
+            title.push(this.state.news[i].title);
+            url.push(this.state.news[i].url);
         }
 
-        var title = this.state.title.map((item, index) => {
-            const authors = this.state.author[index];
-            const urls = this.state.url[index];
-            return (
-                <div>
-                    <a href={urls}><p>{item}</p></a>
-                    <p>{authors}</p>
-                </div>
-            )
-        })
-        
+        const anchorStyle= {
+            color: 'white'
+        }
+
         return(
-            <div>
-                {title}
-            </div>
+            this.state.news ?
+            <div className="newsArticles"> 
+            <TextLoop>
+                <div><a href={url[0]} style={anchorStyle}>{title[0]}</a> {author[0]}</div>
+                <div><a href={url[1]} style={anchorStyle}>{title[1]}</a> {author[1]}</div>
+                <div><a href={url[2]} style={anchorStyle}>{title[2]}</a> {author[2]}</div>
+                <div><a href={url[3]} style={anchorStyle}>{title[3]}</a> {author[3]}</div>
+                <div><a href={url[4]} style={anchorStyle}>{title[4]}</a> {author[4]}</div>
+                <div><a href={url[5]} style={anchorStyle}>{title[5]}</a> {author[5]}</div>
+                <div><a href={url[6]} style={anchorStyle}>{title[6]}</a> {author[6]}</div>
+            </TextLoop> </div> : null
         )
     }
 }
