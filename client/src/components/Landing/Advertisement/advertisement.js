90962fb2d7d64e4db2dad707494d50671d77989c
Arata Kagan <aratakagan@gmail.com>
2019-06-21 01:31:10 -0700

    advertisement fixed

--- client/src/components/Landing/Advertisement/advertisement.js
+++ client/src/components/Landing/Advertisement/advertisement.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import styles from './advertisement.module.css';
 
 function Advertisement(){
-
+    
     return(
-        <div className={styles['main']}>
-            <h1 className={styles['h1']}>Want to Join Us?</h1>
-        </div>
+       <div className='ad-main'>
+            <div className='ad-text-box'>
+                <p>Want to Join Us?</p>
+            </div>
+       </div>
     ) 
 }
 
