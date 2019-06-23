086a1c635a47debc09cabc299a9fb0fab5476b49
Arata Kagan <aratakagan@gmail.com>
2019-06-23 00:51:29 -0700

    card image in about page in progress

--- client/src/components/About/Card/cardList.js
+++ client/src/components/About/Card/cardList.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
-import {Grid, Card} from '@material-ui/core';
+import {Grid, Card, CardActionArea, CardMedia, Typography} from '@material-ui/core';
 import CardData from './card.json';
+import style from './card.module.css';
+import myImage from './qg-test.jpg';
 
 class CardList extends Component {
     render(){
 
         return(
-            <div>
+            <div className={style['card-outer']}>
                 <Grid container>
                     {CardData.map((item, index) => {
                     return(
                         <Grid item xs>
                             <Card style={{ padding: 60, margin: 20, backgroundColor: "#BCBCBE", color: "white"}}>
-                            {item.title}
+                                <CardActionArea>
+                                    <CardMedia 
+                                        className="card-media"
+                                        image="/qg-test.jpg"
+                                        title="card-media"
+                                    />
+                                </CardActionArea>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {item.title}
+                              </Typography>
                             </Card>
                         </Grid>
                         ) 
