5823286d675cfb70c88376084f64bb1eb401975d
Arata Kagan <aratakagan@gmail.com>
2019-07-10 23:24:15 -0700

    contact form in progress

--- client/src/components/ContactUs/contact-us.js
+++ client/src/components/ContactUs/contact-us.js
@@ -6,6 +6,23 @@ function ContactUs(){
         <div>
             <div className={style['contact-us-image']}>
             </div>
+            <div >
+                <h1>Contact Us</h1>
+                <div>
+                    <form action='/action_page.php'>
+                        <label>First Name</label>
+                        <input type='text' className={style['form-input']} id='fname' name='firstname' placeholder='Your first name...' />
+                        <label>Last Name</label>
+                        <input type='text' className={style['form-input']} id='lname' name='lastname' placeholder='Your last name...' />
+                        <label>Email</label>
+                        <input type='email' className={style['form-input']} id='email' name='email' placeholder='Your email' />
+                        <label>Subject</label>
+                        <textarea id='subject' name='subject' placeholder='Write something..'></textarea>
+                        <input type='submit' className={style['form-submit']} value='Submit' />
+                    </form>
+                </div>
+            </div>
+
             
         </div>
     )
