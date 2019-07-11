81ab20893fe4d0d8b4cf2a60581bb70d319b00db
Arata Kagan <aratakagan@gmail.com>
2019-07-11 16:03:31 -0700

    added styling on the contact form

--- client/src/components/ContactUs/contact-us.js
+++ client/src/components/ContactUs/contact-us.js
@@ -6,20 +6,25 @@ function ContactUs(){
         <div>
             <div className={style['contact-us-image']}>
             </div>
-            <div >
-                <h1>Contact Us</h1>
+            <div className={style['contact-us-main']}>
+                <h1 className={style['h1']}>Contact Us</h1>
                 <div>
-                    <form action='/action_page.php'>
-                        <label>First Name</label>
-                        <input type='text' className={style['form-input']} id='fname' name='firstname' placeholder='Your first name...' />
-                        <label>Last Name</label>
-                        <input type='text' className={style['form-input']} id='lname' name='lastname' placeholder='Your last name...' />
-                        <label>Email</label>
-                        <input type='email' className={style['form-input']} id='email' name='email' placeholder='Your email' />
-                        <label>Subject</label>
-                        <textarea id='subject' name='subject' placeholder='Write something..'></textarea>
-                        <input type='submit' className={style['form-submit']} value='Submit' />
-                    </form>
+                    <div className='row'>
+                        <div className='col'>
+                            <form action='/action_page.php' className={style['form-main']}>
+                                <label className={style['label']}>First Name</label>
+                                <input type='text' className={style['form-input']} id='fname' name='firstname' placeholder='Your first name...' />
+                                <label className={style['label']}>Last Name</label>
+                                <input type='text' className={style['form-input']} id='lname' name='lastname' placeholder='Your last name...' />
+                                <label className={style['label']}>Email</label>
+                                <input type='email' className={style['form-input']} id='email' name='email' placeholder='Your email' />
+                                <label className={style['label']}>Subject</label>
+                                <textarea className={style['form-input']} name='subject' placeholder='Write something..'></textarea>
+                                <input type='submit' className={style['form-submit']} value='Submit' />
+                            </form>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
 
