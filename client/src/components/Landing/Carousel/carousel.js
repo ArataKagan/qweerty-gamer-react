d4ec727cb6ca5c0cf4b869ca65c27851e06a0a9c
Arata Kagan <aratakagan@gmail.com>
2019-08-26 20:13:49 -0700

    added responsiveness on event advertisement and sign-up

--- client/src/components/Landing/Carousel/carousel.js
+++ client/src/components/Landing/Carousel/carousel.js
@@ -7,24 +7,22 @@ class Slider extends Component {
     render() {
         return (
             <div className="news-main">
-                <div className="d-flex bd-highlight">
-                        <div className="p-2 flex-fill bd-highlight event">
-                                <div className="d-flex flex-column bd-highlight mb-3 event-inner">
-                                    <p className="p-2 bd-highlight ad-text">What's happening now</p>
-                                    <div>
-                                        <Carousel autoplay='true'>
-                                            <img src="https://i.imgur.com/1EouPxr.png" alt='logo'/>
-                                        </Carousel>
-                                    </div>
-                                </div>
+                    <div className="event">
+                        <div className="event-inner">
+                            <p className="p-2 bd-highlight ad-text">What's happening now</p>
+                            <div>
+                                <Carousel autoplay='true'>
+                                    <img src="https://i.imgur.com/1EouPxr.png" alt='logo'/>
+                                </Carousel>
+                            </div>
                         </div>
-                        <div className="p-2 flex-fill bd-highlight sign-up">
-                                <div className="d-flex flex-column bd-highlight mb-3 sign-up-inner">
-                                    <p className="p-2 bd-highlight text1">Come Join Us</p>
-                                    <p className="p-2 bd-highlight text2">Email</p>
-                                </div>
+                    </div>
+                    <div className="sign-up">
+                        <div className="sign-up-inner">
+                            <p className="p-2 bd-highlight text1">Come Join Us</p>
+                            <p className="p-2 bd-highlight text2">Email</p>
                         </div>
-                </div>
+                    </div>
             </div>
         )
     }
