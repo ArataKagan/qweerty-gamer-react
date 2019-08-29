13dac443651735b84add075905cc9072d571d4c9
Arata Kagan <aratakagan@gmail.com>
2019-08-29 15:51:40 -0700

    added pokemon-go event banner

--- client/src/components/Landing/Carousel/carousel.js
+++ client/src/components/Landing/Carousel/carousel.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Carousel from 'nuka-carousel';
 import style from './carousel.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faMeetup } from '@fortawesome/free-brands-svg-icons'
 
 class Slider extends Component {
     render() {
@@ -12,16 +13,20 @@ class Slider extends Component {
                             <p className="p-2 bd-highlight ad-text">What's happening now</p>
                             <div>
                                 <Carousel autoplay='true'>
-                                    <img src="https://i.imgur.com/1EouPxr.png" alt='logo'/>
+                                    <img src="https://i.imgur.com/ExlV7U0.png" className="pokemon-go-event" alt='logo'/>
                                 </Carousel>
                             </div>
                         </div>
                     </div>
-                    <div className="sign-up">
-                        <div className="sign-up-inner">
-                            <p className="p-2 bd-highlight text1">Come Join Us</p>
-                            <p className="p-2 bd-highlight text2">Email</p>
-                        </div>
+                    <div className="d-flex flex-column bd-highlight social-middle">
+                            <div className="social-middle-inner">
+                                <p className="p-2 bd-highlight text1">Connect With Us</p>
+                                <div className="social-icons-middle">
+                                    <a href='https://www.facebook.com/groups/qweertygamers/'><FontAwesomeIcon icon={faFacebook} className="facebook" color="#eee" /></a>
+                                    <a href='https://twitter.com/qweertygamers'><FontAwesomeIcon icon={faTwitter} className="twitter" color="#eee" /></a>
+                                    <FontAwesomeIcon icon={faMeetup} className="meetup" color="#eee"/>
+                                </div>
+                            </div>
                     </div>
             </div>
         )
