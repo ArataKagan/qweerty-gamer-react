a1c4182bf311c76773ea2d99fb4611ecea675da9
Arata Kagan <aratakagan@gmail.com>
2019-10-27 12:57:40 -0700

    moved member profile to the main about page

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './about.module.scss';
-import CardList from './Card/cardList';
+// import CardList from './Card/cardList';
+import Member from './Member/member';
 
 function About(){
         return (
@@ -10,7 +11,8 @@ function About(){
                 <div className={styles['about-second-half']}>
                     <h1 className={styles['h1']}>About Qweerty Gamers</h1>
                     <p className={styles['about-paragraph']}>Qweerty Gamers is a growing nonprofit 501(c)(3) based in Los Angeles, CA. In 2015, Qweerty existed as a small Facebook Group for a shared Minecraft server. In late 2017, Qweerty focused on a new mission, to "Game for good". Quickly after, Qweerty began launching fundraisers for organizations like the National Center for Lesbian Rights, and the Translatina Coalition. Qweerty's goals were reciprocated in the community quickly. Later in 2018, Qweerty was approached by sympathetic lawyers and helped Qweerty file for nonprofit status. Qweerty works with organizations such as RIOT, Niantic, Blizzard, and other major and independent video game studios to provide free events, workshops, and more.</p>
-                    <CardList />
+                    {/* <CardList /> */}
+                    <Member />
                 </div>
             </div>
         )
