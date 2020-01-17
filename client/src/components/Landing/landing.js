8a623ca3068189dae280e9ca12bcb158d8721217
Arata Kagan <aratakagan@gmail.com>
2020-01-16 23:18:42 -0800

    changed landing page font to bungee and roboto

--- client/src/components/Landing/landing.js
+++ client/src/components/Landing/landing.js
@@ -9,7 +9,7 @@ class Landing extends Component {
         return (
             <div>
                <Advertisement />
-               <Slider />
+               
                <Mission />           
             </div>
         )
