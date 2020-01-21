7c60d2ed41a6d34e056d80961b0fcfdedd504c7c
Arata Kagan <aratakagan@gmail.com>
2020-01-20 23:33:58 -0800

    added instagram and discord social icons on footer

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import styles from './footer.module.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitter, faMeetup } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faTwitter, faMeetup, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons'
 
 class Footer extends Component {
     render(){
+        const pStyle = {
+            paddingRight: '60px'
+        }
+
         return(
             <div className={styles['footer']}>
                 <div className="container">
@@ -24,10 +28,13 @@ class Footer extends Component {
                             </div>
                       </div>
                       <div className={styles["social-icons"]} style={{flex: 0, paddingTop: '20px'}}>
-                            <div className="d-flex flex-column bd-highlight mb-3 social-icons">
+                            <div className="d-flex flex-row bd-highlight mb-3 social-icons">
+                                <p style={pStyle}>CONNECT WITH US</p>
                                 <a href='https://www.facebook.com/groups/qweertygamers/'><FontAwesomeIcon icon={faFacebook} size="2x" color="#eee" /></a>
+                                <a><FontAwesomeIcon icon={faInstagram} size="2x" color="#eee" /></a>
                                 <a href='https://twitter.com/qweertygamers'><FontAwesomeIcon icon={faTwitter} size="2x" color="#eee" /></a>
                                 <a href='https://twitter.com/qweertygamers'><FontAwesomeIcon icon={faMeetup} size="2x" color="#eee"/></a>
+                                <a><FontAwesomeIcon icon={faDiscord} size="2x" color="#eee" /></a>
                             </div>
                       </div>
                    </div>
