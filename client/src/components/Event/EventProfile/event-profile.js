87ad16b20fd7448f666b7134afb36d7de3e7b9dc
Arata Kagan <aratakagan@gmail.com>
2020-02-17 23:34:20 -0800

    adding styling to event information

--- client/src/components/Event/EventProfile/event-profile.js
+++ client/src/components/Event/EventProfile/event-profile.js
@@ -20,14 +20,18 @@ class EventProfile extends Component {
 
     render(){
         return(
-            this.state.eventData.map((item, index) => {
-                return(
-                    <div className={styles["event-data-outer"]}>
-                        <p key={index}>{item.title}</p>
-                        <p key={index + 1}>{item.date}</p>
-                    </div>
-                );
-            })
+            <div className={styles["event-data-outer"]}>
+                <p>UPCOMING EVENTS</p>
+                {this.state.eventData.map((item, index) => {
+                        return(
+                            <div>
+                                <p key={index}>{item.title}</p>
+                                <p key={index + 1}>{item.date}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         )
     }
 }
