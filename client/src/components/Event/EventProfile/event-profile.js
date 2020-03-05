5d4747cd1a459e3f10820c0bd94af560bf80d5c0
Arata Kagan <aratakagan@gmail.com>
2020-03-04 23:52:40 -0800

    fix the position for inner event data

--- client/src/components/Event/EventProfile/event-profile.js
+++ client/src/components/Event/EventProfile/event-profile.js
@@ -21,18 +21,18 @@ class EventProfile extends Component {
     render(){
         return(
             <div className={styles["event-data-outer"]}>
-                <div className={styles["main-text-outer"]}>
-                    <p className={styles["main-text-inner"]}>UPCOMING EVENTS</p>
-                </div>
+                <p className={styles["main-text-inner"]}>UPCOMING EVENTS</p>
+                <div className={styles["event-data-inner"]}>
                 {this.state.eventData.map((item, index) => {
                         return(
-                            <div>
+                            <div className={styles["data"]}>
                                 <p key={index}>{item.title}</p>
                                 <p key={index + 1}>{item.date}</p>
                             </div>
                         )
                     })
                 }
+                </div>
             </div>
         )
     }
