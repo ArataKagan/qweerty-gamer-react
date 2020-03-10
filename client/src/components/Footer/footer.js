5637abb433043d7fecffc5f36dae8c4de9407d93
Arata Kagan <aratakagan@gmail.com>
2020-03-09 22:02:21 -0700

    fixing footer position - in progress

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -6,34 +6,36 @@ import { faFacebook, faTwitter, faMeetup, faInstagram, faDiscord } from '@fortaw
 
 class Footer extends Component {
     render(){
-        const pStyle = {
-            paddingRight: '60px'
-        }
+        // const pStyle = {
+        //     paddingRight: '60px',
+        //     position: "absolute",
+        //     top: "30%",
+        //     whiteSpace: "nowrap"
+        // }
 
         return(
-            <div className={styles['footer']}>
-                <div className={styles["footer-container"]}>
+             <div className={styles['footer']}>
                    <div className="d-flex bd-highlight">
-                      <div className="p-2 flex-fill bd-highlight" style={{textAlign:"center"}}>
-                            <div className={styles['row']}>
-                                <img src={require('./../../assets/images/qg_logo_png.png')} alt='logo' width='200px'/>
-                                <div className="d-flex flex-column bd-highlight mb-3">
-                                    <div className="p-2 bd-highlight">© QWEERTY GAMERS 2019. ALL RIGHTS RESERVED.</div>
-                                    <div className={styles['privacy-policy-link']}>
-                                        <Link to='/privacy-policy' style={{ color: '#eee', fontWeight: 'bolder' }}>
-                                            <div>Privacy Policy</div>
+                      <div className="p-2 flex-fill bd-highlight">
+                            <div className={styles["logo-inner"]}>
+                                <div className={styles['row']}>
+                                    <img src={require('./../../assets/images/qg_logo_png.png')} alt='logo' width='200px'/>
+                                    <div className="d-flex flex-column bd-highlight mb-3">
+                                        <div className="p-2 bd-highlight">© QWEERTY GAMERS 2019. ALL RIGHTS RESERVED.</div>
+                                        <Link to='/privacy-policy'>
+                                            <div className={styles["privacy-policy"]}>Privacy Policy</div>
                                         </Link>
                                     </div>
                                 </div>
                             </div>
                       </div>
-                      <div className="p-2 flex-fill bd-highlight">
+                      <div className="p-2 flex-fill bd-highlight" style={{position:"relative"}}>
                             <div className={styles['row']}>
-                                    <p style={pStyle}>CONNECT WITH US</p>
+                                    <p className={styles["connect-with-us"]}>CONNECT WITH US</p>
                             </div>
                       </div>
-                      <div className="p-2 flex-fill bd-highlight">
-                            <div className={styles["social-icons"]} style={{flex: 0, paddingTop: '20px'}}>
+                      <div className="p-2 flex-fill bd-highlight" style={{position:"relative"}}>
+                            <div className={styles["social-icons"]} style={{flex: 0, paddingTop: '20px', position:"absolute", top:"13%"}}>
                                     <div className="d-flex flex-row bd-highlight mb-3 social-icons">
                                         <a href='https://www.facebook.com/groups/qweertygamers/'><FontAwesomeIcon icon={faFacebook} size="2x" color="#eee" /></a>
                                         <a><FontAwesomeIcon icon={faInstagram} size="2x" color="#eee" /></a>
@@ -44,9 +46,7 @@ class Footer extends Component {
                             </div>
                       </div>
                    </div>
-                </div>
-               
-            </div>
+             </div>
 
         );
     }
