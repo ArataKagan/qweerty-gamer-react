cb1cbd45f9fcb97156d84c41226c776f26df2a6d
Arata Kagan <aratakagan@gmail.com>
2020-03-22 14:55:52 -0700

    added bootstrap grid, added connect with us and fixed padding

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -15,10 +15,9 @@ class Footer extends Component {
 
         return(
              <div className={styles['footer']}>
-                   <div className="d-flex bd-highlight">
-                      <div className="p-2 flex-fill bd-highlight">
-                            <div className={styles["logo-inner"]}>
-                                <div className={styles['row']}>
+                   <div className="row">
+                      <div className="col-md-4 text-center text-md-left">
+                                <div className={styles['first-row']}>
                                     <img src={require('./../../assets/images/qg_logo_png.png')} alt='logo' width='200px'/>
                                     <div className="d-flex flex-column bd-highlight mb-3">
                                         <div className="p-2 bd-highlight">© QWEERTY GAMERS 2020. ALL RIGHTS RESERVED.</div>
@@ -27,24 +26,21 @@ class Footer extends Component {
                                         </Link>
                                     </div>
                                 </div>
-                            </div>
                       </div>
-                      <div className="p-2 flex-fill bd-highlight" style={{position:"relative"}}>
-                            <div className={styles['row']}>
-                                    <p className={styles["connect-with-us"]}>CONNECT WITH US</p>
-                            </div>
+                      <div className="col-md-4 text-center text-md-left">
+                        <div className={styles['second-row']}>
+                            <p>CONNECT WITH US</p>
+                         </div>
                       </div>
-                      <div className="p-2 flex-fill bd-highlight" style={{position:"relative"}}>
-                            <div className={styles["social-icons"]} style={{flex: 0, paddingTop: '20px', position:"absolute", top:"13%"}}>
-                                    <div className="d-flex flex-row bd-highlight mb-3 social-icons">
-                                        <a href='https://www.facebook.com/groups/qweertygamers/'><FontAwesomeIcon icon={faFacebook} size="2x" color="#eee" /></a>
-                                        <a href='https://www.instagram.com/qweertygamers/'><FontAwesomeIcon icon={faInstagram} size="2x" color="#eee" /></a>
-                                        <a href='https://twitter.com/qweertygamers'><FontAwesomeIcon icon={faTwitter} size="2x" color="#eee" /></a>
-                                        <a href='https://www.meetup.com/qweertygamers'><FontAwesomeIcon icon={faMeetup} size="2x" color="#eee"/></a>
-                                        <a href='https://www.twitch.tv/qweertygamers'><FontAwesomeIcon icon={faTwitch} size="2x" color="#eee" /></a>
-                                        <a href='https://discord.gg/wjg4JUC'><FontAwesomeIcon icon={faDiscord} size="2x" color="#eee" /></a>
-                                    </div>
-                            </div>
+                      <div className="col-md-4 text-center text-md-left" style={{position:"relative"}}>
+                        <div className={styles['third-row']}>
+                                <a href='https://www.facebook.com/groups/qweertygamers/'><FontAwesomeIcon icon={faFacebook} size="2x" color="#eee" /></a>
+                                <a href='https://www.instagram.com/qweertygamers/'><FontAwesomeIcon icon={faInstagram} size="2x" color="#eee" /></a>
+                                <a href='https://twitter.com/qweertygamers'><FontAwesomeIcon icon={faTwitter} size="2x" color="#eee" /></a>
+                                <a href='https://www.meetup.com/qweertygamers'><FontAwesomeIcon icon={faMeetup} size="2x" color="#eee"/></a>
+                                <a href='https://www.twitch.tv/qweertygamers'><FontAwesomeIcon icon={faTwitch} size="2x" color="#eee" /></a>
+                                <a href='https://discord.gg/wjg4JUC'><FontAwesomeIcon icon={faDiscord} size="2x" color="#eee" /></a>
+                         </div>
                       </div>
                    </div>
              </div>
