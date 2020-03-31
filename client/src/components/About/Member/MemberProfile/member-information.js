d66676a5fed00c2e2a0c4987147fd79eb3abfd63
Arata Kagan <aratakagan@gmail.com>
2020-03-30 18:26:40 -0700

    added Chandler and deleted Colby in the member

--- client/src/components/About/Member/MemberProfile/member-information.js
+++ client/src/components/About/Member/MemberProfile/member-information.js
@@ -1,37 +1,40 @@
 export default [
-    {
-        id: 1,
-        name: "Raymond Lancione",
-        title: "CEO/President",
-        description: "Raymond Lancione is the President and CEO of Qweerty Gamers. Ray has defined his abilities at game developer studios like Nexon and Pearl Abyss with a background in social media, player relations, content creation, and influencer management.In 2018, Ray officially formed Qweerty Gamers, a nonprofit 501(c)(3) with several goals to benefit the LGBTQ community: 1. Provide the local LGBTQ community friendly gaming events, where they can feel comfortable to integrate into the general gaming community. 2. Provide gaming related opportunities or workshops for LGBTQ people and allies for upcoming industry trends (IE. Streaming, Shoutcasting, etc)",
-        image: require('../../../../assets/images/member-profile-image/ray.jpeg')
-    },
-    {
-        id: 2,
-        name: "Chris Ruben Avila",
-        title: "Staff Volunteer",
-        description: "Christopher (Chris) Avila has been a volunteer for Qweerty Gamers since January 2019. Along with volunteering at various events, Chris is also the Qweerty seamstress, he designs and creates apparel and accessories for volunteers and for merchandise. He/she/they identify as non-binary and responds to all pronouns as well as considers themselves as queer. He graduated from Citrus College in June 2019 with degrees in business administration, social and behavioral science, biological and physical science, language arts and mathematics. When not helping out with Qweerty or in class, he drives for Lyft part-time or volunteers at other organizations such as the LA LGBT Center or Kitten Rescue. He is currently training to run in the 2020 LA marathon to raise money for Kitten Rescue, a no-kill shelter for cats run by volunteers who work to find forever loving homes for cats and kittens. He's a huge Disney fan and takes any opportunity to visit the parks. Having been inspired by Disney's approach to their local cat community, he aspires to one day start his own non-profit no-kill cat sanctuary.",
-        image: require('../../../../assets/images/member-profile-image/chris.jpg')
-    },
-    {
-        id: 3,
-        name: "Kirby Conner",
-        title: "Staff Volunteer",
-        description: "Connor discovered Qweerty Gamers after searching for a gaymer community that wasn’t limited to talking through screens. Welcoming an opportunity to expand his knowledge of community building, Kirby quickly volunteered to help moderate the Qweerty Social Media pages by creating engaging content, as well as help organize events. Equipped with his knowledge of video game culture and history, Kirby hopes to help make Qweerty a positive space for both Gamers and the LGBT community alike through valuable in-person meetups.",
-        image: require('../../../../assets/images/member-profile-image/kirby.jpeg')
-    },
-    {
-        id: 4,
-        name: 'Joe Song',
-        title: "Graphic Designer",
-        description: "A recently added member to Qweerty Gamers, Joseph Song is also their head graphic designer as well as one of the artists for Qweerty gamers as of April of 2019. Joseph is an art student who hopes to one day break into the gaming industry in the graphics or art department. Using his skills with digital illustrations programs to produce banners and flyers for upcoming events for Qweerty. When not helping out with Qweerty, Joseph is usually at school or at home taking care of his 2 cats.",
-        image: require('../../../../assets/images/member-profile-image/joe.jpg')
-    },
-    {
-        id: 5,
-        name: 'Arata Kagan',
-        title: "Web Developer",
-        image: require('../../../../assets/images/member-profile-image/arata.jpeg')
-    }
-]
-    
+  {
+    id: 1,
+    name: 'Raymond Lancione',
+    title: 'CEO/President',
+    description:
+      'Raymond Lancione is the President and CEO of Qweerty Gamers. Ray has defined his abilities at game developer studios like Nexon and Pearl Abyss with a background in social media, player relations, content creation, and influencer management.In 2018, Ray officially formed Qweerty Gamers, a nonprofit 501(c)(3) with several goals to benefit the LGBTQ community: 1. Provide the local LGBTQ community friendly gaming events, where they can feel comfortable to integrate into the general gaming community. 2. Provide gaming related opportunities or workshops for LGBTQ people and allies for upcoming industry trends (IE. Streaming, Shoutcasting, etc)',
+    image: require('../../../../assets/images/member-profile-image/ray.jpeg')
+  },
+  {
+    id: 2,
+    name: 'Chris Ruben Avila',
+    title: 'Treasurer and CFO',
+    description:
+      "Christopher (Chris) Avila has been a volunteer for Qweerty Gamers since January 2019. Along with volunteering at various events, Chris is also the Qweerty seamstress, he designs and creates apparel and accessories for volunteers and for merchandise. He/she/they identify as non-binary and responds to all pronouns as well as considers themselves as queer. He graduated from Citrus College in June 2019 with degrees in business administration, social and behavioral science, biological and physical science, language arts and mathematics. When not helping out with Qweerty or in class, he drives for Lyft part-time or volunteers at other organizations such as the LA LGBT Center or Kitten Rescue. He is currently training to run in the 2020 LA marathon to raise money for Kitten Rescue, a no-kill shelter for cats run by volunteers who work to find forever loving homes for cats and kittens. He's a huge Disney fan and takes any opportunity to visit the parks. Having been inspired by Disney's approach to their local cat community, he aspires to one day start his own non-profit no-kill cat sanctuary.",
+    image: require('../../../../assets/images/member-profile-image/chris.jpg')
+  },
+  {
+    id: 3,
+    name: 'Joe Song',
+    title: 'Graphic Designer',
+    description:
+      'A recently added member to Qweerty Gamers, Joseph Song is also their head graphic designer as well as one of the artists for Qweerty gamers as of April of 2019. Joseph is an art student who hopes to one day break into the gaming industry in the graphics or art department. Using his skills with digital illustrations programs to produce banners and flyers for upcoming events for Qweerty. When not helping out with Qweerty, Joseph is usually at school or at home taking care of his 2 cats.',
+    image: require('../../../../assets/images/member-profile-image/joe.jpg')
+  },
+  {
+    id: 4,
+    name: 'Arata Kagan',
+    title: 'Web Developer',
+    image: require('../../../../assets/images/member-profile-image/arata.jpeg')
+  },
+  {
+    id: 5,
+    name: 'Chandler Poling',
+    title: 'Staff Volunteer',
+    description:
+      'Chandler Poling is the Co-Founder of White Bear PR, a Public Relations firm specializing in Publicity for Composers, Songwriters, Music Supervisors, and Film & Music Festivals around the world.  Throughout his career, Chandler has run successful Oscar, Golden Globe, Grammy, BAFTA and Emmy campaigns securing nominations and wins for his clients, including the history-making campaign for Hildur Gudnadóttir and her scores from Joker and Chernobyl.  His clients have been featured in national and international publications, such as Time Magazine, Vanity Fair, New York Times, Variety, Hollywood Reporter, The Guardian, LA Times, and Rolling Stone Magazine to name just a few. He created the first-ever composer-focused panel at Comic-Con San Diego, the World’s Largest Pop Culture Convention.',
+    image: require('../../../../assets/images/member-profile-image/Chandler.jpg')
+  }
+];
