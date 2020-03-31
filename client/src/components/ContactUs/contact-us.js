bdef55229273bb5024b9f7402b308354ea8cb374
Arata Kagan <aratakagan@gmail.com>
2020-03-30 18:14:00 -0700

    added main group photo, changed contact email address

--- client/src/components/ContactUs/contact-us.js
+++ client/src/components/ContactUs/contact-us.js
@@ -1,79 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import style from './contact-us.module.scss';
 import axios from 'axios';
 
 class ContactUs extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            fname: '',
-            lname: '',
-            email: '',
-            message: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: '',
+      lname: '',
+      email: '',
+      message: ''
+    };
 
-        // this.resetForm = this.resetForm.bind(this);
-    }
+    // this.resetForm = this.resetForm.bind(this);
+  }
 
-    handleFormSubmit(e){
-        e.preventDefault();
-        console.log(this.state);
-        console.log('submit clicked');
-        const data = {
-            "fname": this.state.fname,
-            "lname": this.state.lname,
-            "email": this.state.email,
-            "message": this.state.message
-        }
-        
-        // fetch("http://localhost:3000/api/form", {
-        //     method: "POST",
-        //     headers: {"Content-Type": "application/json",
-        //               "Access-Control-Allow-Origin": "*"
-        //              },
-        //     body: JSON.stringify(data)
-        // })
-        // .then((res) => {return res.json()})
-        // .then(res => {
-        //     if(res.ok){
-        //         console.log("request sucess");
-        //         return res.json();
-        //     } else {
-        //         console.log("request fail");
-        //         throw new Error("something went wrong..");
-        //     }
-        // }).catch(err => {
-        //     console.log(err);
-        //     console.log("error happed");
-        // }) 
+  handleFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+    console.log('submit clicked');
+    const data = {
+      fname: this.state.fname,
+      lname: this.state.lname,
+      email: this.state.email,
+      message: this.state.message
+    };
 
-        axios.post("http://localhost:3001/contact", data)
-            .then(
-                response => {
-                    console.log("success!");
-                    console.log(response.data);
-            })
-            .catch(err => {
-                console.log(err);
-                console.log(err.message);
-                console.log(err.stack);
-            })
-       
-        this.setState({
-            fname: '',
-            lname: '',
-            email: '',
-            message: ''
-        })
-    }
+    // fetch("http://localhost:3000/api/form", {
+    //     method: "POST",
+    //     headers: {"Content-Type": "application/json",
+    //               "Access-Control-Allow-Origin": "*"
+    //              },
+    //     body: JSON.stringify(data)
+    // })
+    // .then((res) => {return res.json()})
+    // .then(res => {
+    //     if(res.ok){
+    //         console.log("request sucess");
+    //         return res.json();
+    //     } else {
+    //         console.log("request fail");
+    //         throw new Error("something went wrong..");
+    //     }
+    // }).catch(err => {
+    //     console.log(err);
+    //     console.log("error happed");
+    // })
 
+    axios
+      .post('http://localhost:3001/contact', data)
+      .then(response => {
+        console.log('success!');
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+        console.log(err.stack);
+      });
 
-    render(){
-        return(
-            <div>
-                <div className={style['contact-us-main']}>
-                    <p className={style['get-in-touch']}>GET IN TOUCH</p>
-                    {/* <div>
+    this.setState({
+      fname: '',
+      lname: '',
+      email: '',
+      message: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className={style['contact-us-main']}>
+          <p className={style['get-in-touch']}>GET IN TOUCH</p>
+          {/* <div>
                         <div className='row'>
                             <div className='col'>
                                 <form className={style['form-main']} method="POST" onSubmit={e => this.handleFormSubmit(e)} action="/contact">
@@ -100,19 +99,18 @@ class ContactUs extends Component {
                                 </form>
                             </div>
                         </div>
-                    </div> */} 
-                    <div className={style['text']}>
-                        Please email us to the following address for any inquiries:
-                    </div>
-                    <div className={style['email-address']}>
-                        ray@qweertygamers.org 
-                    </div>
-                    <div className={style['contact-us-image']}></div>
-                </div>
-            </div>
-        )
-    }
+                    </div> */}
+          <div className={style['text']}>
+            Please email us to the following address for any inquiries:
+          </div>
+          <div className={style['email-address']}>
+            contact@qweertygamers.org
+          </div>
+          <div className={style['contact-us-image']}></div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default ContactUs;
