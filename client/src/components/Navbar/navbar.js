42f63c62cc29d6fb3bdacb73f16c381935dd0739
Arata Kagan <aratakagan@gmail.com>
2020-03-30 18:37:14 -0700

    added shopping link in the navbar

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,95 +1,142 @@
 import React, { Component } from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitter, faMeetup } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faTwitter,
+  faMeetup
+} from '@fortawesome/free-brands-svg-icons';
 import styles from './navbar.module.css';
 
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink 
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
 } from 'reactstrap';
 
 class Navigation extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isHover: false,
-            isOpen: false
-        };
-    }
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isHover: false,
+      isOpen: false
+    };
+  }
 
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
-    // onMouseEnter = (e) => {
-    //    console.log('this is: ', e.target);
-    //    this.setState({
-    //        isHover: e.target
-    //    })
-    // } 
+  // onMouseEnter = (e) => {
+  //    console.log('this is: ', e.target);
+  //    this.setState({
+  //        isHover: e.target
+  //    })
+  // }
 
-    // onMouseLeave = () => {
-    //     console.log('mouse left')
-    //     this.setState({
-    //         isHover: false
-    //     })
-    // }
+  // onMouseLeave = () => {
+  //     console.log('mouse left')
+  //     this.setState({
+  //         isHover: false
+  //     })
+  // }
 
-    render() {
+  render() {
+    const navStyle = {
+      color: '#ffffff',
+      fontFamily: 'Bungee',
+      textDecoration: 'none'
+    };
 
-        const navStyle = {
-            color: "#ffffff",
-            fontFamily: 'Bungee',
-            textDecoration: 'none'
-        }
+    const donateNavStyle = {
+      color: '#C4D759',
+      fontFamily: 'Bungee',
+      textDecoration: 'none',
+      border: '2px solid #C4D759',
+      boxSizing: 'border-box',
+      borderRadius: '13px'
+    };
 
-        const donateNavStyle = {
-            color: '#C4D759',
-            fontFamily: 'Bungee',
-            textDecoration: 'none',
-            border: '2px solid #C4D759',
-            boxSizing: 'border-box',
-            borderRadius: '13px'
-        }
-    
-        return (
-            <div>
-                <Navbar style={{backgroundColor: '#1d1e22'}} light expand="md">
-                <NavbarBrand href="/" className={styles['main-logo']}><img src={require('./../../assets/images/qg_logo_png.png')} alt="logo" width='200px'/></NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/" style={navStyle} className={styles['nav-item']}>HOME</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/event" style={navStyle} className={styles['nav-item']}>EVENTS</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/about" style={navStyle} className={styles['nav-item']}>ABOUT</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/contact-us" style={navStyle} className={styles['nav-item']}>CONTACT US</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://tiltify.com/qweerty-gamers" style={donateNavStyle} className={styles['nav-item']}>DONATE</NavLink>
-                            </NavItem>
-                    </Nav>
-                </Collapse>
-                </Navbar> 
-        </div>
-            
-        );
-    }
+    return (
+      <div>
+        <Navbar style={{ backgroundColor: '#1d1e22' }} light expand='md'>
+          <NavbarBrand href='/' className={styles['main-logo']}>
+            <img
+              src={require('./../../assets/images/qg_logo_png.png')}
+              alt='logo'
+              width='200px'
+            />
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink
+                  href='/'
+                  style={navStyle}
+                  className={styles['nav-item']}
+                >
+                  HOME
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href='/event'
+                  style={navStyle}
+                  className={styles['nav-item']}
+                >
+                  EVENTS
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href='/about'
+                  style={navStyle}
+                  className={styles['nav-item']}
+                >
+                  ABOUT
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href='https://www.etsy.com/shop/qweertygamers/'
+                  style={navStyle}
+                  className={styles['nav-item']}
+                >
+                  SHOPPING
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href='/contact-us'
+                  style={navStyle}
+                  className={styles['nav-item']}
+                >
+                  CONTACT US
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href='https://tiltify.com/qweerty-gamers'
+                  style={donateNavStyle}
+                  className={styles['nav-item']}
+                >
+                  DONATE
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default Navigation;
