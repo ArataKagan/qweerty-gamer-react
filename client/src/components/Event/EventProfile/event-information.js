68202509e20b303968aabfe15436b9786c52dc32
Arata Kagan <aratakagan@gmail.com>
2020-04-19 20:07:36 -0700

    added game night event

--- client/src/components/Event/EventProfile/event-information.js
+++ client/src/components/Event/EventProfile/event-information.js
@@ -1,6 +1,17 @@
 export default [
   {
     id: 1,
+    image: require('./qg_game_night.jpg'),
+    title: 'Qweerty Gamers Gamenight - Overwatch',
+    date: 'Saturday, April 25, 2020',
+    time: '7 PM – 9 PM (PST)',
+    venue: 'Online',
+    address1: '',
+    address2: '',
+    url: 'https://discord.gg/xH55rzb'
+  },
+  {
+    id: 2,
     image: require('./artjam2.png'),
     title: 'Art Jam - Free Qweerty Online Class',
     date: 'Sunday, April 19, 2020',
