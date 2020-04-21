d30ca84046faa261362cc8a81e4d00827efd6116
Arata Kagan <aratakagan@gmail.com>
2020-04-20 18:18:44 -0700

    deleted art jam event

--- client/src/components/Event/EventProfile/event-information.js
+++ client/src/components/Event/EventProfile/event-information.js
@@ -9,16 +9,5 @@ export default [
     address1: '',
     address2: '',
     url: 'https://discord.gg/xH55rzb'
-  },
-  {
-    id: 2,
-    image: require('./artjam2.png'),
-    title: 'Art Jam - Free Qweerty Online Class',
-    date: 'Sunday, April 19, 2020',
-    time: '7 PM – 9 PM (PST)',
-    venue: 'Online',
-    address1: '',
-    address2: '',
-    url: 'https://www.twitch.tv/qweertygamers'
   }
 ];
