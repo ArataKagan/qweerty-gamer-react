7b69c3a5e5360fca288df85b26795e71a9e26775
Arata Kagan <aratakagan@gmail.com>
2020-05-09 21:08:27 -0700

    added animal crossing event

--- client/src/components/Event/EventProfile/event-information.js
+++ client/src/components/Event/EventProfile/event-information.js
@@ -9,5 +9,16 @@ export default [
     address1: '',
     address2: '',
     url: 'https://discord.gg/wjg4JUC'
+  },
+  {
+    id: 2,
+    image: require('./animal-crossing-0517.jpg'),
+    title: 'Qweerty Gamers Game Night - Animal Crossing',
+    date: 'Saturday, May 16, 2020',
+    time: '7 PM – 9 PM (PST)',
+    venue: 'Online',
+    address1: '',
+    address2: '',
+    url: 'https://discord.gg/wjg4JUC'
   }
 ];
