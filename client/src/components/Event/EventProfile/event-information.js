f36bcb24e237b6e805e2fd7eb1082c878b7d5aff
Arata Kagan <aratakagan@gmail.com>
2020-05-10 17:08:10 -0700

    deleted minecraft event

--- client/src/components/Event/EventProfile/event-information.js
+++ client/src/components/Event/EventProfile/event-information.js
@@ -1,17 +1,6 @@
 export default [
   {
     id: 1,
-    image: require('./minecraft-0509.jpg'),
-    title: 'Qweerty Gamers Game Night - MINECRAFT',
-    date: 'Saturday, May 9, 2020',
-    time: '7 PM – 9 PM (PST)',
-    venue: 'Online',
-    address1: '',
-    address2: '',
-    url: 'https://discord.gg/wjg4JUC'
-  },
-  {
-    id: 2,
     image: require('./animal-crossing-0517.jpg'),
     title: 'Qweerty Gamers Game Night - Animal Crossing',
     date: 'Saturday, May 16, 2020',
