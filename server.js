6eae55520771f17b3100e6bf1f62c0868a1f2314
Arata Kagan <aratakagan@gmail.com>
2020-05-18 22:24:20 -0700

    fix the bug

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/users', require('./routes/users'));
+// app.use('/api/auth', require('./routes/auth'));
 
 // Serve static assets if it's in production
 if (process.env.NODE_ENV === 'production') {
