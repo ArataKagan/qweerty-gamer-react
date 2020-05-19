bd996a3b912bab63404f5a76e6d4c5a73eeefe96
Arata Kagan <aratakagan@gmail.com>
2020-05-18 22:14:18 -0700

    fixed the error

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const connectDB = require('./config/db');
 connectDB();
 
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('API running'));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
