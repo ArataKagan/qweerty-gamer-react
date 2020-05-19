e7bc12bff400bceb8cb56592510a073c3ceb80a4
Arata Kagan <aratakagan@gmail.com>
2020-05-18 21:48:08 -0700

    added overwatch event

--- client/src/components/Event/EventProfile/event-information.js
+++ client/src/components/Event/EventProfile/event-information.js
@@ -1,13 +1,13 @@
 export default [
   {
     id: 1,
-    image: require('./animal-crossing-0517.jpg'),
-    title: 'Qweerty Gamers Game Night - Animal Crossing',
-    date: 'Saturday, May 16, 2020',
+    image: require('./overwatch-0523.jpg'),
+    title: 'Qweerty Gamers Game Night - Overwatch',
+    date: 'Saturday, May 23, 2020',
     time: '7 PM – 9 PM (PST)',
     venue: 'Online',
     address1: '',
     address2: '',
-    url: 'https://discord.gg/wjg4JUC'
+    url: 'https://www.twitch.tv/qweertygamers'
   }
 ];
