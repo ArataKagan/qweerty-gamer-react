8e8065cdf95639d54c8022e7e2e01e57e20eb9c7
Arata Kagan <aratakagan@gmail.com>
2020-05-18 22:56:47 -0700

    reverting to previous state

--- client/src/components/Event/EventProfile/event-information.js
+++ client/src/components/Event/EventProfile/event-information.js
@@ -1,30 +1,9 @@
 export default [
   {
     id: 1,
-<<<<<<< HEAD
     image: require('./overwatch-0523.jpg'),
     title: 'Qweerty Gamers Game Night - Overwatch',
     date: 'Saturday, May 23, 2020',
-||||||| f36bcb2... deleted minecraft event
-    image: require('./animal-crossing-0517.jpg'),
-    title: 'Qweerty Gamers Game Night - Animal Crossing',
-    date: 'Saturday, May 16, 2020',
-=======
-    image: require('./minecraft-0509.jpg'),
-    title: 'Qweerty Gamers Game Night - MINECRAFT',
-    date: 'Saturday, May 9, 2020',
-    time: '7 PM – 9 PM (PST)',
-    venue: 'Online',
-    address1: '',
-    address2: '',
-    url: 'https://discord.gg/wjg4JUC'
-  },
-  {
-    id: 2,
-    image: require('./animal-crossing-0517.jpg'),
-    title: 'Qweerty Gamers Game Night - Animal Crossing',
-    date: 'Saturday, May 16, 2020',
->>>>>>> parent of f36bcb2... deleted minecraft event
     time: '7 PM – 9 PM (PST)',
     venue: 'Online',
     address1: '',
