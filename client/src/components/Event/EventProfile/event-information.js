5e5bfe42181e5ff56e79338be2447514c532517a
Arata Kagan <aratakagan@gmail.com>
2020-06-02 21:13:20 -0700

    deleted event

--- client/src/components/Event/EventProfile/event-information.js
+++ client/src/components/Event/EventProfile/event-information.js
@@ -1,17 +1,6 @@
 export default [
   {
     id: 1,
-    image: require('./QG-live-interview.jpg'),
-    title: 'Qweerty Pride Interview with Dream Daddy Dev Leighton Gray',
-    date: 'Monday, June 1st, 2020',
-    time: '7 PM – 8 PM (PST)',
-    venue: 'Online',
-    address1: '',
-    address2: '',
-    url: 'https://www.twitch.tv/qweertygamers'
-  },
-  {
-    id: 2,
     image: require('./qg-gamenight-0606.jpg'),
     title: 'Qweerty Gamers Game Night - Dead by Daylight',
     date: 'Saturday, June 6th, 2020',
